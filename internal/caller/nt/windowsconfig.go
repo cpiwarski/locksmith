6734352597ccaf132e3ed8792ecb234bd638b63b
Chris Piwarski <christopher.piwarski@betterhelp.com>
2020-04-14 17:29:38 -0700

    staticcheck fixes

--- internal/caller/nt/windowsconfig.go
+++ internal/caller/nt/windowsconfig.go
@@ -1,24 +1,24 @@
 package caller
 
+// WindowsConfig represents a WireGuard configuration
 type WindowsConfig struct {
 	DeviceName string
-	org string
-	Config []byte
+	Config     []byte
 }
 
 // NewWindowsConfig creates a new Windows configuration.
-func NewConfig() WindowsConfig {
+func NewConfig() *WindowsConfig {
 	return new(WindowsConfig)
 }
 
 // InstallConfig installs the configuration to the Windows device.
-func (w WindowsConfig) InstallConfig() error {
+func (w *WindowsConfig) InstallConfig() error {
 	//TODO: Complete method
 	return nil
 }
 
 // UninstallConfig removes the configuration from the Windows device.
-func (w WindowsConfig) UninstallConfig() error {
+func (w *WindowsConfig) UninstallConfig() error {
 	//TODO: Complete method
 	return nil
 }
